2b458646ddc3f7ef1e93ce7b95449969437684e7
Ercan Tomaç <ercan2243ercan@gmail.com>
2024-02-12 22:03:11 +0100

    Binary to decimal converter and Abstract Syntax Tree

---
+++ AbstractSyntaxTree/Program.fs
@@ -0,0 +1,44 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+type ASTNode =
+    | Binary of BinaryOperation
+    | Constant of int
+
+and BinaryOperation =
+    | Add of (ASTNode * ASTNode)
+    | Subtract of (ASTNode * ASTNode)
+    | Multiply of (ASTNode * ASTNode)
+    | Divide of (ASTNode * ASTNode)
+
+
+let rec Evaluate (node: ASTNode) : int =
+    match node with
+    | Binary operation -> EvaluateBinaryOperation operation
+    | Constant c -> c
+
+and EvaluateBinaryOperation (operation: BinaryOperation) : int =
+    match operation with
+    | Add (left, right) -> Evaluate left + Evaluate right
+    | Subtract (left, right) -> Evaluate left - Evaluate right
+    | Multiply (left, right) -> Evaluate left * Evaluate right
+    | Divide (left, right) -> Evaluate left / Evaluate right
+
+
+let Sum (value: ASTNode) (add: ASTNode) : ASTNode = Binary(Add(add, value))
+
+let Difference (value: ASTNode) (subtract: ASTNode) : ASTNode = Binary(Subtract(subtract, value))
+
+let Product (value: ASTNode) (multiply: ASTNode) : ASTNode = Binary(Multiply(multiply, value))
+
+let Quotient (value: ASTNode) (divide: ASTNode) : ASTNode = Binary(Divide(divide, value))
+
+let equation =
+    Constant 2
+    |> Sum(Constant 5)
+    |> Product(Constant 12)
+    |> Quotient(Constant 6)
+    |> Sum(Constant 17)
+
+let result = Evaluate equation
+
+printfn $"{result} :d"
---
+++ BinaryToDecimal/Program.fs
@@ -0,0 +1,35 @@
+﻿let rec power (number: int) (pow: int) =
+    match pow with
+    | pow when pow < 0 -> 0
+    | 0 -> 1
+    | 1 -> number
+    | _ -> number * power number (pow - 1)
+
+let getDecimalValue (input: (int * char)) =
+    let (index, bit) = input
+
+    match bit with
+    | '1' -> power 2 index
+    | _ -> 0
+
+let binToDec (binaryString: string) =
+    let reversed = binaryString.ToCharArray() |> Array.rev
+    let reversedList = Seq.toList reversed |> List.indexed
+
+    reversedList
+    |> List.fold (fun accumulation element -> accumulation + getDecimalValue element) 0
+
+let a = binToDec "00000000"
+printfn $"{a}"
+let b = binToDec "00000001"
+printfn $"{b}"
+let c = binToDec "00000010"
+printfn $"{c}"
+let d = binToDec "00000011"
+printfn $"{d}"
+let e = binToDec "00000100"
+printfn $"{e}"
+let f = binToDec "00000101"
+printfn $"{f}"
+let g = binToDec "00000110"
+printfn $"{g}"
